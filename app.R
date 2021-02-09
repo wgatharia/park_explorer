2b69ee662414fc9e5874be25ee68febcaf548478
William Gatharia <WGatharia@casey.org>
2021-02-09 11:55:36 -0800

    new project

---
+++ app.R
@@ -0,0 +1,105 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+
+source("helpers.R")
+
+# Import data
+annual_visits <- read_rds("data/annual_visits.rds")
+monthly_visits <- read_rds("data/monthly_visits.rds")
+
+parks <- annual_visits$park_name %>% unique()
+
+ui <- fluidPage(theme = shinytheme("sandstone"),
+                titlePanel("National Park Visits Explorer"),
+  fluidRow(
+    column(4, 
+           selectInput("selected_park", "Park", choices = parks,
+                       selected = "Crater Lake NP")
+    ),
+    column(4,
+           sliderInput("selected_year", "Year", 
+                       value = 2019,
+                       min = min(annual_visits$year), 
+                       max = max(annual_visits$year),
+                       step = 1,
+                       sep = "")
+    )
+  ),
+  textOutput("park_name"),
+  h2("Annual Visits"),
+  fluidRow(
+    column(8,
+           plotOutput("annual_plot")
+    ),
+    column(4,
+           textOutput("park_summary")
+    )
+  ),
+  h2("Monthly Visits"),
+  fluidRow(
+    column(8, 
+           plotOutput("monthly_plot"),
+           checkboxInput("display_average", "Display monthly average?")
+    ),
+    column(4, 
+           tableOutput("monthly_table")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$park_name <- renderText(input$selected_park)
+  
+  # Subset data to selected park --------------------------------------------
+  
+  annual_data <- reactive({
+    annual_visits %>% 
+      filter(park_name == input$selected_park)
+  })
+  
+  monthly_data <- reactive({
+    monthly_visits %>% 
+      filter(park_name == input$selected_park) 
+  })
+  
+  # Outputs that use a single year ------------------------------------------
+  
+  output$park_summary <- renderText({
+    annual_data() %>% 
+      filter(year == input$selected_year) %>% 
+      summarize_park()
+  })
+  
+  output$monthly_table <- renderTable(digits = 0, {
+    monthly_data() %>% 
+      filter(year == input$selected_year) %>% 
+      select(month_name, recreation_visits)
+  })
+  
+  
+  # Outputs that use all years ----------------------------------------------
+  
+  output$annual_plot <- renderPlot({
+    annual_data() %>% 
+      plot_annual(highlight_year = input$selected_year)
+  })
+  
+  output$monthly_plot <- renderPlot({
+    monthly_data() %>% 
+      plot_monthly(display_average = input$display_average,
+                   highlight_year = input$selected_year) 
+  })
+  
+}
+
+shinyApp(ui, server)
---
+++ helpers.R
@@ -0,0 +1,57 @@
+
+# Plotting color defaults -------------------------------------------------
+
+my_colors <- c(
+  light = "#F8F5F0",
+  dark = "#3E3F3A",
+  blue = "#325D88",
+  grey = "#8E8C84"
+)
+
+theme_set(theme_grey(base_size = 18))
+theme_update(
+  panel.background = element_rect(fill = my_colors["light"]), 
+  text = element_text(color = my_colors["dark"])
+)
+
+update_geom_defaults("line", list(color = my_colors["dark"]))
+
+# Function to form a one sentence summary from a year
+# of annual data
+summarize_park <- function(one_year){
+  comma <- scales::label_comma()
+  one_year %>% 
+    glue::glue_data(
+      "In { year }, { park_name } had { comma(recreation_visits) } recreation visits."
+    )
+}
+
+# Output functions --------------------------------------------------------
+
+
+# Takes annual data and produces a plot
+plot_annual <- function(annual_data, highlight_year = 2019){
+  annual_data %>% 
+    ggplot(aes(year, recreation_visits)) +
+    geom_point(data = ~ filter(., year == highlight_year)) +
+    geom_line() +
+    scale_y_continuous(labels = scales::label_comma()) +
+    labs(x = "", y = "Visits")
+}
+
+# Takes monthly data and produces a plot
+plot_monthly <- function(monthly_data, highlight_year = 2019,
+                         display_average = TRUE){
+  p <- monthly_data %>% 
+    ggplot(aes(month, recreation_visits_proportion)) +
+    geom_line(aes(group = year), alpha = 0.2) +
+    geom_line(data = ~ filter(.x, year == highlight_year)) +
+    scale_x_continuous(breaks = 1:12, labels = month.abb) +
+    scale_y_continuous(labels = scales::label_percent()) +
+    labs(x = "", y = "Of Annual Visits")
+  if(display_average) {
+    p <- p + stat_summary(fun = mean, 
+                          geom = "line", color = "#325D88", size = 1.5) 
+  }
+  p
+}
